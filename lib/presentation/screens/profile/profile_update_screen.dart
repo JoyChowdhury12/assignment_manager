fe4c53c1db673130a4114f813aa81be2f34f238b
Joy Chowdhury <147070186+JoyChowdhury12@users.noreply.github.com>
2024-08-05 19:07:59 +0600

    Constructing on update profile screen page

--- lib/presentation/screens/profile/profile_update_screen.dart
+++ lib/presentation/screens/profile/profile_update_screen.dart
@@ -1,3 +1,4 @@
+import 'package:assignment_manager/presentation/utils/app_colors.dart';
 import 'package:assignment_manager/presentation/widgets/app_background_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -6,7 +7,18 @@ class UpdateProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.themeColor,
+        iconTheme: IconThemeData(color: Colors.white),
+        title: Text(
+          "Update Profile",
+          style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                color: Colors.white,
+                fontSize: 22,
+              ),
+        ),
+      ),
       body: AppBackgroundWidget(
           child: Center(
         child: Text("Update Profile"),
--- lib/presentation/widgets/task_item.dart
+++ lib/presentation/widgets/task_item.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TaskItem extends StatelessWidget {
@@ -8,19 +7,32 @@ class TaskItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       color: Colors.white,
-      margin: EdgeInsets.symmetric(horizontal: 24, vertical: 5),
+      margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 5),
       child: Padding(
-        padding: EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
+            const Text(
               "Task Title",
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            Text("Task subtitle/caption"),
+            const Text("Task subtitle/caption"),
             Row(
-              children: [Chip(label: Text("New"))],
+              children: [
+                const Chip(
+                  label: Text("New"),
+                ),
+                const Spacer(),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.edit),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.delete),
+                ),
+              ],
             )
           ],
         ),
